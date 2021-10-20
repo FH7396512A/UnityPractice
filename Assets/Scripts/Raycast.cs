943c775c9e3d3b829796c4967c9e67841ccf5475
FH7396512A <83833075+FH7396512A@users.noreply.github.com>
2021-10-20 21:45:39 +0200

    add Collision, Raycast

---
+++ Assets/Scripts/Raycast.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycast : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            Debug.DrawRay(Camera.main.transform.position, ray.direction * 10.0f, Color.red, 1.0f);
+        }
+    }
+}
